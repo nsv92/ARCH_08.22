e3e0b9bb223fc89b9afc38e7f88a8e444e454cc8
Sergei <svnat92@gmail.com>
2022-09-01 22:23:02 +0300

    Дописал UserMapper
    Дописал update, insert, delete

--- geek-architecture-02/examples/src/main/java/ru/geekbrains/orm/UserMapper.java
+++ geek-architecture-02/examples/src/main/java/ru/geekbrains/orm/UserMapper.java
@@ -14,12 +14,21 @@ public class UserMapper {
 
     private final PreparedStatement selectUser;
 
+    private final PreparedStatement insertUser;
+
+    private final PreparedStatement updateUser;
+
+    private final PreparedStatement deleteUser;
+
     private final Map<Long, User> identityMap = new HashMap<>();
 
     public UserMapper(Connection conn) {
         this.conn = conn;
         try {
             this.selectUser = conn.prepareStatement("select id, username, password from users where id = ?");
+            this.insertUser = conn.prepareStatement("insert into users (username, password) values (?, ?)");
+            this.updateUser = conn.prepareStatement("update users set username = ?, password = ? where id = ?");
+            this.deleteUser = conn.prepareStatement("delete from users where id = ?");
         } catch (SQLException e) {
             throw new IllegalStateException(e);
         }
@@ -46,13 +55,36 @@ public class UserMapper {
 
     public void update(User user) {
 
+        try {
+            updateUser.setString(1, user.getLogin());
+            updateUser.setString(2, user.getPassword());
+            updateUser.setLong(3, user.getId());
+            updateUser.executeQuery();
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
+        identityMap.put((long) user.getId(), user);
     }
 
     public void insert(User user) {
 
+        try {
+            insertUser.setString(1, user.getLogin());
+            insertUser.setString(2, user.getPassword());
+            insertUser.executeQuery();
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
     }
 
     public void delete(User user) {
 
+        try {
+            deleteUser.setLong(1, user.getId());
+            deleteUser.executeQuery();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        identityMap.remove((long)user.getId());
     }
 }
