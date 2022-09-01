de9ae25dd59c3060d5744940a27d09c00daac882
Sergei <svnat92@gmail.com>
2022-09-02 00:22:34 +0300

    User repository

--- geek-architecture-02/examples/src/main/java/ru/geekbrains/orm/UserRepository.java
+++ geek-architecture-02/examples/src/main/java/ru/geekbrains/orm/UserRepository.java
@@ -1,6 +1,8 @@
 package ru.geekbrains.orm;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Optional;
 
 public class UserRepository {
@@ -11,37 +13,63 @@ public class UserRepository {
 
     private final UnitOfWork unitOfWork;
 
+    private final PreparedStatement startTransaction;
+
+    private final PreparedStatement commitTransaction;
+
+    private final PreparedStatement rollbackTransaction;
+
     public UserRepository(Connection conn) {
         this.conn = conn;
         this.mapper = new UserMapper(conn);
         this.unitOfWork = new UnitOfWork(mapper);
+        try {
+            this.startTransaction = conn.prepareStatement("start transaction;");
+            this.commitTransaction = conn.prepareStatement("commit;");
+            this.rollbackTransaction = conn.prepareStatement("rollback;");
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
     }
 
     public Optional<User> findById(long id) {
-        return Optional.empty();
+        return mapper.findById(id);
     }
 
     public void beginTransaction() {
-
+        try {
+            startTransaction.executeQuery();
+            unitOfWork.commit();
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
     }
 
     public void insert(User user) {
-
+        unitOfWork.registerNew(user);
     }
 
     public void update(User user) {
-
+        unitOfWork.registerUpdate(user);
     }
 
     public void delete(User user) {
-
+        unitOfWork.registerDelete(user);
     }
 
     public void commitTransaction() {
-
+        try {
+            commitTransaction.executeQuery();
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
     }
 
     public void rollbackTransaction() {
-
+        try {
+            rollbackTransaction.executeQuery();
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
     }
 }
